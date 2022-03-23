a64581b35d028fb48d13c05cab749734cddd9357
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-23 18:59:28 +0530

    [Madhavee] Adding Usecase - 2

--- CsharpEmpWageComputation/Program.cs
+++ CsharpEmpWageComputation/Program.cs
@@ -13,17 +13,23 @@ namespace CsharpEmpWageComputation
             Console.WriteLine("Welcome to Employee Wage Compuatation Program\n");
 
             const int isPresent = 1;
-
+            const int wagePerHour = 20;
+            int empWorkHour = 0;
+            int empDailyWage = 0;
             Random random = new Random();
             int check = random.Next(0, 2);
             if(check == isPresent)
             {
                 Console.WriteLine("Employee is Present");
+                empWorkHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empWorkHour = 0;
             }
+            empDailyWage = empWorkHour*wagePerHour;
+            Console.WriteLine($"\nEmployee Daily Wage : {empDailyWage}\n");
             Console.ReadLine();
         }
     }
