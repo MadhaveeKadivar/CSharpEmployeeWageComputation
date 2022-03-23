476db2c87cc50e21f18db764e3f211d0f872d1b8
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-23 18:51:45 +0530

    [Madhavee] Adding Usecase - 1

--- CsharpEmpWageComputation/Program.cs
+++ CsharpEmpWageComputation/Program.cs
@@ -10,6 +10,21 @@ namespace CsharpEmpWageComputation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Employee Wage Compuatation Program\n");
+
+            const int isPresent = 1;
+
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if(check == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
