947e46ed52b3ca5806165391ef1a8614a191b591
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-26 22:57:11 +0530

    Update EmpWageComputation.cs

--- CsharpEmpWageComputation/EmpWageComputation.cs
+++ CsharpEmpWageComputation/EmpWageComputation.cs
@@ -11,12 +11,10 @@ namespace CsharpEmpWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Compuatation Program\n");
-
             const int isPresent = 1;
-
-            Random random = new Random();
-            int check = random.Next(0, 2);
-            if(check == isPresent)
+            Random random = new Random(); // Creating object of Random class
+            int check = random.Next(0, 2); // Generating random number 0 or 1
+            if(check == isPresent) // Checking that employee is present or not
             {
                 Console.WriteLine("Employee is Present");
             }
